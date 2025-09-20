ba05d0d69985df6dba6419e37e1d164e8034b365
SharafAi <sharafhassan33@gmail.com>
2025-09-20 14:02:12 +0500

    exit sfter handlinng one request

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http  = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  process.exit(); // Exit after handling one request
+
 });
 
 const PORT = 3000;
