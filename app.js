fde6fbf8942d98b4cf7be624ce8570890ab2a0d4
SharafAi <sharafhassan33@gmail.com>
2025-09-20 14:57:26 +0500

    sending response

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http  = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>mvcrate</title></head>');
+  res.write('<body><h1>unbox maldives</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 const PORT = 3001;
