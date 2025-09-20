56bade576c458fd6864777ada2b2d4272fd20ca7
SharafAi <sharafhassan33@gmail.com>
2025-09-21 02:19:14 +0500

    taking user input

---
+++ user.js
@@ -0,0 +1,37 @@
+const http  = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+
+if (req.url === '/') {  
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title> mvcrate </title></head>');
+  res.write('<body><h1> enter your details: </h1>'); //1
+  res.write('<form action ="/submit-details" method="POST">'); //2
+  res.write('<input type="text" name="username" placeholder="enter your name"><br>');//3
+  
+  res.write('<label for="male"> male </label>') //4
+  res.write('<input type="radio" id = "male" name= "gender" value = "male" />'); //5
+  res.write('<label for="female"> female </label>'); //6
+  res.write('<input type="radio" id = "female" name= "gender" value = "female" />'); //7
+  res.write('<br><input type="submit" value="submit">'); //8
+  res.write('</form>');
+  
+  res.write('</body>');
+  res.write('</html>');
+  return res.end();
+}
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>mvcrate</title></head>');
+  res.write('<body><h1>unbox maldives</h1></body>');
+  res.write('</html>');
+  return res.end();
+});
+
+const PORT = 3003;
+server.listen(PORT,() => {
+  console.log (`Server is running on http://localhost:${PORT}`)
+});
+ 
