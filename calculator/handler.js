695134ccee86ae2e07372dbbe482ac221fcd6955
SharafAi <sharafhassan33@gmail.com>
2025-09-23 02:24:34 +0500

    practice set-sum-calculator

---
+++ calculator/handler.js
@@ -0,0 +1,50 @@
+const { sumRequestHandler } = require("./sum");
+
+
+
+const requestHandler = (req, res) => {
+  console.log(req.url, req.method);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+   <html>
+   <head><title> practice set </title></head>
+   <body><h1> welcome to calculator </h1>
+   <a href="/calculator"> go to calculator </a>
+  </body>
+  </html>
+    `);
+    return res.end();
+  } else if (req.url.toLowerCase() === "/calculator") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+   <html>
+   <head><title> practice set </title></head>
+   <body><h1> here is the calculator </h1>
+   <form action="/calculate-result" method="POST">
+   <input type="text" placeholder="first number" name="first"/>
+    <input type="text" placeholder="second number" name="second"/>
+    <input type="submit" value="sum"/>
+    </form>
+  </body>
+  </html>
+    `);
+    return res.end();
+  } else if (req.url.toLowerCase() === "/calculate-result" && req.method === "POST") {
+    return sumRequestHandler(req, res);
+  }
+
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(`
+   <html>
+   <head><title> practice set </title></head>
+   <body><h1> 404 page does not exist </h1>
+   <a href="/"> go to home </a>
+  </body>
+  </html>
+    `);
+  return res.end();
+};
+
+exports.requestHandler = requestHandler;
---
+++ calculator/sum.js
@@ -0,0 +1,24 @@
+const sumRequestHandler = (req, res) => {
+  console.log("in sum request handler", req.url, req.method);
+  const body = [];
+  req.on("data", chunk => body.push(chunk));
+  req.on("end", () => {
+    const bodyStr = Buffer.concat(body).toString();
+    const params = new URLSearchParams(bodyStr);
+    const bodyObj = Object.fromEntries(params);
+    const result = Number(bodyObj.first) + Number(bodyObj.second);
+    console.log(result);
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+   <html>
+   <head><title> practice set </title></head>
+   <body><h1> your result is ${result} </h1>
+   <a href="/"> go to home </a>
+  </body>
+  </html>`);
+    return res.end();
+  })
+
+}
+
+exports.sumRequestHandler = sumRequestHandler;
