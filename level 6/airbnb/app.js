1997e3f14628734da3b98521177aeaac2ec5c3fc
SharafAi <sharafhassan33@gmail.com>
2025-09-26 09:34:01 +0500

    adding html files

--- level 6/airbnb/app.js
+++ level 6/airbnb/app.js
@@ -12,10 +12,9 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use(hostRouter);
 
-
-
-
-
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404! Your page not found on airbnb</h1>');
+})
 
 
 
