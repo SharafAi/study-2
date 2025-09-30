d21ce182f6400bb0b4a1af6d05303960e3b0ed57
SharafAi <sharafhassan33@gmail.com>
2025-09-30 13:31:47 +0500

    errors fixed - reading data from files

--- level 8/mvc/controllers/homes.js
+++ level 8/mvc/controllers/homes.js
@@ -1,24 +1,23 @@
-const { RergisterdHomes } = require("../routes/hostRouter");
-const Home = require("../models/home"); // import the class
+const Home = require("../models/home"); // import class
 
 exports.getaddHome = (req, res, next) => {
   res.render('addHome', { pageTitle: 'Add Home', currentPage: 'addHome' });
-}
+};
 
 exports.postaddHome = (req, res, next) => {
   const { houseName, price, location, rating, photoURL } = req.body;
-  
-  const home = new Home(houseName, price, location, rating, photoURL); // create instance
-  home.save(); // call save on the instance
+  const home = new Home(houseName, price, location, rating, photoURL); // instance
+  home.save(); // push to array
 
   res.render('homeadded', { pageTitle: 'Home added successfully', currentPage: 'homeadded' });
-}
+};
 
 exports.gethomes = (req, res, next) => {
-  const RergisterdHomes = Home.fetchAll(RergisterdHomes => res.render("home", {
-    RergisterdHomes: RergisterdHomes,
-    pageTitle: 'Airbnb Home',
-    currentPage: 'home'
-  })); 
-  
-}
+  Home.fetchAll((RergisterdHomes) =>
+    res.render("home", {
+      RergisterdHomes: RergisterdHomes,
+      pageTitle: 'Airbnb Home',
+      currentPage: 'home'
+    })
+  );
+};
--- level 8/mvc/models/home.js
+++ level 8/mvc/models/home.js
@@ -1,8 +1,10 @@
+//core modulees
 const fs = require('fs');
 const path = require('path');
 const rootDir = require('../utilities/pathUtil');
 
-module.exports = class Home { // Capitalized class name
+
+module.exports = class Home { 
   constructor(houseName, price, location, rating, photoURL){
     this.houseName = houseName;
     this.price = price;
@@ -12,18 +14,19 @@ module.exports = class Home { // Capitalized class name
   }
   
   save() {
-    RergisterdHomes.push(this);
-    const homeDataPath = path.join(rootDir, 'data', 'homes.json');
-    fs.writeFile(homeDataPath, JSON.stringify(RergisterdHomes), error => {
-      console.log("file writing concluded", error);
-    })
+    Home.fetchAll((RergisterdHomes) => {
+      RergisterdHomes.push(this);
+      const homeDataPath = path.join(rootDir, 'data', 'homes.json');
+      fs.writeFile(homeDataPath, JSON.stringify(RergisterdHomes), (error) => {
+        console.log("file writing concluded", error);
+      })
+    });
   }
 
   static fetchAll(callback) {
     const homeDataPath = path.join(rootDir, 'data', 'homes.json');
     fs.readFile(homeDataPath, (err, data) => {
-      console.log("file read:", err, data);
-      callback(!err ? JSON.parse(data) : [])
+      callback(!err ? JSON.parse(data) : []);
     });
   }
 }
